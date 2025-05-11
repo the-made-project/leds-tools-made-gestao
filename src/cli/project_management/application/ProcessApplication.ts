047fcc728d94df5e69915b6715869ede8785cc87
BrenoAmancio <brenoaaffonso@gmail.com>
2025-05-11 20:42:09 -0300

    Resto de dos builders e suas implementações

--- src/cli/project_management/application/ProcessApplication.ts
+++ src/cli/project_management/application/ProcessApplication.ts
@@ -1,9 +1,9 @@
-
 import { Activity, isProcess, Model, Task, Process } from "../../../language/generated/ast.js";
 import { AbstractApplication } from "./AbstractApplication.js";
 import { Process as ProcessData, Activity as ActivityData, Task as TaskData} from "made-report-lib";
 import { ProcessBuilder } from "./builders/ProcessBuilder.js";
-
+import { TaskBuilder } from "./builders/TaskBuilder.js";
+import { ActivityBuilder } from "./builders/ActivityBuilder.js";
 
 export  class ProcessApplication extends AbstractApplication {
 
@@ -36,25 +36,23 @@ export  class ProcessApplication extends AbstractApplication {
     }
 
     private async createActivity (activity: Activity): Promise<ActivityData>{
+        const builder = new ActivityBuilder()
 
-        return {
-            id: activity.id ?? "",  
-            name: activity.name ?? "",
-            description: activity.description ?? "",             
-            tasks:  await Promise.all(activity.tasks.map (async task => await this.createTask(task)) ?? []) 
+        builder.setId(activity.id ?? "")
+            .setName(activity.name ?? "")
+            .setDescription(activity.description ?? "")
+            .setTasks(await Promise.all(activity.tasks.map (async task => await this.createTask(task)) ?? []))
             //depends: await Promise.all(activity.depend)
-        }
+        return builder.build()
     }
-
-    
 
     private async createTask (task: Task): Promise<TaskData>{
-        return {
-            id: task.id ?? "",  
-            name: task.name ?? "" ,
-            description: task.description ?? "" 
-        }
+        const builder = new TaskBuilder()
+
+        builder.setId(task.id ?? "")
+            .setName(task.name ?? "")
+            .setDescription(task.description ?? "")
+            
+        return builder.build()
     }
-  
-       
 }
